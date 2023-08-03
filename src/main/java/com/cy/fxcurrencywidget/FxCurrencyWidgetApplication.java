339cc1a6a8dc6030242dd588d586d61df06f6548
cyrilcp <cpangilinan@kloudgin.com>
2023-08-03 20:28:44 +0800

    Initial commit - Spring Initializr project generation

---
+++ src/main/java/com/cy/fxcurrencywidget/FxCurrencyWidgetApplication.java
@@ -0,0 +1,13 @@
+package com.cy.fxcurrencywidget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxCurrencyWidgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxCurrencyWidgetApplication.class, args);
+    }
+
+}

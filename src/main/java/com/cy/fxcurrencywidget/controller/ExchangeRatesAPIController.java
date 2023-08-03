6bd9b3bc6d2a4a501772458252d85cd6f95019b6
cyrilcp <cpangilinan@kloudgin.com>
2023-08-03 21:59:26 +0800

    Add other criteria and tests

--- src/main/java/com/cy/fxcurrencywidget/controller/ExchangeRatesAPIController.java
+++ src/main/java/com/cy/fxcurrencywidget/controller/ExchangeRatesAPIController.java
@@ -1,14 +1,13 @@
 package com.cy.fxcurrencywidget.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/currency")
@@ -21,8 +20,51 @@ public class ExchangeRatesAPIController {
 
     @GetMapping("/symbols")
     public ResponseEntity<?> getSymbols() {
-        ResponseEntity<String> response = restTemplate.getForEntity("/symbols?access_key=" + apiKey, String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity(UriComponentsBuilder
+                .fromPath("/symbols")
+                .queryParam("access_key", apiKey)
+                .build()
+                .toUriString(), String.class);
         log.info("getSymbols {} ",response.getBody());
         return response;
     }
+
+    @GetMapping("/convert")
+    public ResponseEntity<?> convert(@RequestParam("from") String from,
+                                     @RequestParam("to") String to,
+                                     @RequestParam("amount") String amount) throws Exception {
+        if (StringUtils.isEmpty(from)) {
+            throw new Exception("from currency is required");
+        }
+        if (StringUtils.isEmpty(to)) {
+            throw new Exception("to currency is required");
+        }
+        if (StringUtils.isEmpty(amount)) {
+            throw new Exception("amount is required");
+        }
+        ResponseEntity<String> response = restTemplate.getForEntity(UriComponentsBuilder
+                .fromPath("/convert")
+                .queryParam("access_key", apiKey)
+                .queryParam("from", from)
+                .queryParam("to", to)
+                .queryParam("amount", amount)
+                .build()
+                .toUriString(), String.class);
+        log.info("convert {} ",response.getBody());
+        return response;
+    }
+    @GetMapping("/latest/{currency}")
+    public ResponseEntity<?> getLatestRate(@PathVariable("currency") String currency) {
+        ResponseEntity<String> response = restTemplate.getForEntity(UriComponentsBuilder
+                .fromPath("/latest")
+                .queryParam("access_key", apiKey)
+                .queryParam("symbols", currency)
+                .build()
+                .toUriString(), String.class);
+        log.info("getLatestRate {} ",response.getBody());
+        return response;
+    }
+
+
+
 }
--- src/test/java/com/cy/fxcurrencywidget/controller/ExchangeRatesAPIControllerIntegrationTest.java
+++ src/test/java/com/cy/fxcurrencywidget/controller/ExchangeRatesAPIControllerIntegrationTest.java
@@ -1,16 +1,12 @@
 package com.cy.fxcurrencywidget.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ExchangeRatesAPIControllerIntegrationTest {
@@ -25,31 +21,40 @@ public class ExchangeRatesAPIControllerIntegrationTest {
     @Test
     public void getSymbols() {
         ResponseEntity<?> response = exchangeRatesAPIController.getSymbols();
+        is200OKResponse(response);
+    }
+
+    @Test
+    public void convert() throws Exception {
+        ResponseEntity<?> response = exchangeRatesAPIController.convert("USD", "JPY", "25");
+        is200OKResponse(response);
+    }
+    @Test
+    public void convertWithExceptionMissingAmount() {
+        assertThrows(
+                Exception.class,
+                () -> exchangeRatesAPIController.convert("USD", "JPY", ""));
+    }
+    @Test
+    public void convertWithExceptionMissingFrom() {
+        assertThrows(
+                Exception.class,
+                () -> exchangeRatesAPIController.convert("", "JPY", "25"));
+    }
+    @Test
+    public void convertWithExceptionMissingTo() {
+        assertThrows(
+                Exception.class,
+                () -> exchangeRatesAPIController.convert("USD", "", "25"));
+    }
+    @Test
+    public void getLatestForCurrency() {
+        ResponseEntity<?> response = exchangeRatesAPIController.getLatestRate("USD");
+        is200OKResponse(response);
+    }
+
+    private static void is200OKResponse(ResponseEntity<?> response) {
         assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-    @Test
-    public void buyCurrency() {
-
-    }
-    @Test
-    public void sellCurrency() {
-
-    }
-    @Test
-    public void buyAmount() {
-
-    }
-    @Test
-    public void sellAmount() {
-
-    }
-    @Test
-    public void getAllRates() {
-
-    }
-    @Test
-    public void getSelectedRate() {
-
     }
 }

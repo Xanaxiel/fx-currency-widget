c4bf3b4aa364658a48bf498675e407a99fdaf27d
cyrilcp <cpangilinan@kloudgin.com>
2023-08-03 20:35:39 +0800

    Add RestTemplate

--- src/test/java/com/cy/fxcurrencywidget/FxCurrencyWidgetApplicationTests.java
+++ src/test/java/com/cy/fxcurrencywidget/FxCurrencyWidgetApplicationTests.java
@@ -1,13 +1,21 @@
 package com.cy.fxcurrencywidget;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class FxCurrencyWidgetApplicationTests {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+        assertNotNull(restTemplate);
     }
 
 }
